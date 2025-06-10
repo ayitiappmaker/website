527baf8ebdbd818386fe6b771c90702842ddbba5
God'sgift Uko <godsgiftuko@gmail.com>
2025-06-10 01:46:53 +0100

    Update metadata

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,4 @@
 export class Configs {
     static appName = 'SanteNow';
+    static appDescription = 'The app aims to bridge the healthcare accessibility gap in Haiti by providing immediate, AI-powered medical guidance while respecting cultural context and language preferences.';
 }
